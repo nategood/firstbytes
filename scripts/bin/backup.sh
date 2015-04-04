adf6bb979771e8a1fefa0c3a7adf186b9ac3bcb5
Nathan <nate.good@showclix.com>
2015-04-04 10:25:36 -0400

    latest backup cp. lazy version. no symlink.

--- scripts/bin/backup.sh
+++ scripts/bin/backup.sh
@@ -1,5 +1,6 @@
 #! /bin/sh
 cd ~/backups
 mongodump
-tar -czf `date '+%s'`.tgz dump
+tar -czf latest.tgz dump
+cp latest.tgz `date '+%s'`.tgz
 # todo: scp, push to S3 or something off of this server
